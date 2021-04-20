93695c90c7aa38a31b186a6e48704465474eed24
David Marquillas <dmarquillas@gmail.com>
2021-04-19 21:26:27 -0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { SharedModule } from './components/shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, InicioComponent],
+  imports: [BrowserModule, AppRoutingModule, SharedModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/inicio/inicio.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.component.html',
+  styleUrls: ['./inicio.component.scss'],
+})
+export class InicioComponent implements OnInit {
+  error: boolean = false;
+  pin: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  ingresar() {
+    if (!this.pin) {
+      this.error = true;
+
+      setTimeout(() => {
+        this.error = false;
+      }, 3000);
+    }
+  }
+}
