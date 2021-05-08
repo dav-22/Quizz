8daedbed70b2ff06198ccb125cf976cfae155987
David Marquillas <dmarquillas@gmail.com>
2021-05-08 18:15:03 -0300

    avances

--- src/app/components/usuario/usuario-routing.module.ts
+++ src/app/components/usuario/usuario-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RecuperarPasswordComponent } from './recuperar-password/recuperar-password.component';
 
-const routes: Routes = [{ path: '', component: LoginComponent }];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'recuperarPassword', component: RecuperarPasswordComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
